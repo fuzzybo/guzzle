c7fc1d6ed987898f04b97d5b59b42af8d4fbf9e5
Steve Cooke <steve@integrityconsultants.com.au>
2025-11-10 08:33:22 +1100

    Update phar-stub.php
    use Symfony\Contracts\EventDispatcher\Event as SymfonyEvent; instead of Symfony\Component\EventDispatcher

--- phar-stub.php
+++ phar-stub.php
@@ -4,7 +4,7 @@ Phar::mapPhar('guzzle.phar');
 
 require_once 'phar://guzzle.phar/vendor/symfony/class-loader/Symfony/Contracts/ClassLoader/UniversalClassLoader.php';
 
-$classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
+$classLoader = new Symfony\Contracts\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
     'Guzzle' => 'phar://guzzle.phar/src',
     'Symfony\\Contracts\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher',
