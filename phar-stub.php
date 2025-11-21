287efee0f6e873c27e9d744b36b1381487a26933
Steve Cooke <steve@integrityconsultants.com.au>
2025-11-21 13:04:32 +1100

    Update phar-stub.php

--- phar-stub.php
+++ phar-stub.php
@@ -4,10 +4,10 @@ Phar::mapPhar('guzzle.phar');
 
 require_once 'phar://guzzle.phar/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-$classLoader = new Symfony\Contracts\ClassLoader\UniversalClassLoader();
+$classLoader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $classLoader->registerNamespaces(array(
     'Guzzle' => 'phar://guzzle.phar/src',
-    'Symfony\\Contracts\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher',
+    'Symfony\\Component\\EventDispatcher' => 'phar://guzzle.phar/vendor/symfony/event-dispatcher',
     'Doctrine' => 'phar://guzzle.phar/vendor/doctrine/common/lib',
     'Monolog' => 'phar://guzzle.phar/vendor/monolog/monolog/src'
 ));
